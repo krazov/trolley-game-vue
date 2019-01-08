d5612975287ac5c3becb1c4e9dcfeb4ad0fede22
Jon Krazov <krazov@interia.pl>
2019-01-08 19:26:02 +0100

    Adding eslint and other stuff

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    pages:{
-        index: {
-            entry: './src/main.ts',
-            title: 'Trolley Dilemma: The Game',
-        },
+  pages: {
+    index: {
+      entry: './src/main.ts',
+      title: 'Trolley Dilemma: The Game',
     },
-    lintOnSave: process.env.NODE_ENV !== 'production',
+  },
+  lintOnSave: process.env.NODE_ENV !== 'production',
 };
